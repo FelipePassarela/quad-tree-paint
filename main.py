f133d0c480a474d202fc381144bd869ff5d9b4e4
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2025-06-22 03:03:47 -0300

    refactor: add stats display

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ WHITE = (245, 233, 223)
 BLACK = (107, 85, 66)
 GRAY = (207, 188, 180)
 RED = (245, 67, 40)
+FONT_SIZE = 20
 
 def draw_quadtree(surface, qt):
     for i in range(len(qt)):
@@ -24,9 +25,17 @@ def draw_particles(surface, qt):
 
 
 def draw_fps(surface, fps):
-    font = pygame.font.SysFont("Arial", 24)
+    font = pygame.font.SysFont("Arial", FONT_SIZE)
     fps_text = font.render(f"FPS: {int(fps)}", True, BLACK)
     surface.blit(fps_text, (10, 10))
+
+
+def draw_counters(surface, particles_count, qt):
+    font = pygame.font.SysFont("Arial", FONT_SIZE)
+    count_text = font.render(f"Particles: {particles_count}", True, BLACK)
+    qt_size_text = font.render(f"Quadrants: {len(qt)}", True, BLACK)
+    surface.blit(count_text, (10, 40))
+    surface.blit(qt_size_text, (10, 70))
 
 
 def main():
@@ -39,31 +48,34 @@ def main():
     clock = pygame.time.Clock()
 
     qtree = QuadTree(0, 0, WIDTH, HEIGHT)
+    particles_count = 0
 
     while running:
-        clock.tick(60)
+        clock.tick(120)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONUP:
                 px, py = event.pos
-                qtree.insert(px, py)
+                particles_count += qtree.insert(px, py)
             if event.type  == pygame.KEYDOWN:
                 if event.key == pygame.K_r:
                     qtree.clear()
+                    particles_count = 0
                 if event.key == pygame.K_ESCAPE:
                     running = False
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] or pygame.mouse.get_pressed()[0]:
             px, py = pygame.mouse.get_pos()
-            qtree.insert(px, py)
+            particles_count += qtree.insert(px, py)
 
         SURFACE.fill(WHITE)
         draw_quadtree(SURFACE, qtree)
         draw_particles(SURFACE, qtree)
         draw_fps(SURFACE, clock.get_fps())
+        draw_counters(SURFACE, particles_count, qtree)
         pygame.display.update()
 
     pygame.quit()
