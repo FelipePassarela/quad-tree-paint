fff19d6e57642d1fca60bb5fa50e3c4c52c468f1
FelipePassarela <126916744+FelipePassarela@users.noreply.github.com>
2025-06-22 00:00:00 -0300

    refactor insert method in QuadTree to simplify node handling and improve clarity

--- quad_tree_array.py
+++ quad_tree_array.py
@@ -15,41 +15,38 @@ class QuadTree:
         current_idx = 0  # Root
 
         while True:
-            current_node = self.nodes[current_idx]
+            node = self.nodes[current_idx]
 
-            if not current_node.is_leaf:
-                quadrant_idx = self._find_quadrant(px, py, current_idx)
-                current_idx = current_node.children_idx[quadrant_idx]
+            if not node.is_leaf:
+                quadrant_idx = self._find_quadrant(px, py, node)
+                current_idx = node.children_idx[quadrant_idx]
                 continue
 
-            is_empty = (current_node.px == None)
+            is_empty = (node.px == None)
             if is_empty:
-                current_node.px = px
-                current_node.py = py
+                node.px = px
+                node.py = py
                 break
             else:
                 # Avoid deep recursion by checking if the particle is already in the node
-                dist_sqr = (current_node.px - px) ** 2 + (current_node.py - py) ** 2
+                dist_sqr = (node.px - px) ** 2 + (node.py - py) ** 2
                 if dist_sqr < 1e-6:
                     break
 
-                self._subdivide(current_idx)
+                self._subdivide(node)
 
                 # Insert old particle in properly quadrant
-                old_px = current_node.px
-                old_py = current_node.py
-                current_node.px = None
-                current_node.py = None
+                old_px, old_py = node.px, node.py
+                node.px, node.py = None, None
 
-                quadrant_idx = self._find_quadrant(old_px, old_py, current_idx)
-                child_idx = current_node.children_idx[quadrant_idx]
-                child_node = self.nodes[child_idx]
-                child_node.px = old_px
-                child_node.py = old_py
+                quadrant_idx = self._find_quadrant(old_px, old_py, node)
+                child_idx = node.children_idx[quadrant_idx]
+                child = self.nodes[child_idx]
+                child.px, child.py = old_px, old_py
 
-    def _find_quadrant(self, px, py, node_idx):
-        mid_x = self.nodes[node_idx].x + self.nodes[node_idx].w // 2
-        mid_y = self.nodes[node_idx].y + self.nodes[node_idx].h // 2
+    def _find_quadrant(self, px, py, node):
+        mid_x = node.x + node.w // 2
+        mid_y = node.y + node.h // 2
 
         if px < mid_x:
             target_idx = 0 if py < mid_y else 2
@@ -57,13 +54,13 @@ class QuadTree:
             target_idx = 1 if py < mid_y else 3
         return target_idx
 
-    def _subdivide(self, node_idx):
-        self.nodes[node_idx].children_idx = list(range(len(self.nodes), len(self.nodes) + 4))
-        self.nodes[node_idx].is_leaf = False
+    def _subdivide(self, node):
+        node.children_idx = list(range(len(self.nodes), len(self.nodes) + 4))
+        node.is_leaf = False
 
-        x, y = self.nodes[node_idx].x, self.nodes[node_idx].y
-        half_w = self.nodes[node_idx].w // 2
-        half_h = self.nodes[node_idx].h // 2
+        x, y = node.x, node.y
+        half_w = node.w // 2
+        half_h = node.h // 2
 
         self.nodes += [
             Node(x, y, half_w, half_h),                    # NE
